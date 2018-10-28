9122e06b1c33f1b3a2e4111ba6dc4ebca355ff52
Keine <ckeine@healthcare.uiowa.edu>
2018-10-28 10:41:58 -0500

    update

--- RsCorrection.m
+++ RsCorrection.m
@@ -1,7 +1,7 @@
 function [dataCorrected] = RsCorrection(data, Rs, Cm, Vhold, Vrev, SR, fraction)
 
 % performs offline series resistance correction for recorded currents
-% Input: 
+% Input:
 
 % - data = recorded current trace, can be single vector or multiple recordings as array or cell
 % - Rs = uncompensated series resistance (Rs) during the recording in Ohm, i.e. if the Rs during the experiment was 10 MOhm and online compensated by 50% by
@@ -15,13 +15,13 @@ function [dataCorrected] = RsCorrection(data, Rs, Cm, Vhold, Vrev, SR, fraction)
 % Based on: "Traynelis SF (1998) Software-based correction of single
 % compartment series resistance errors. J Neurosci Methods 86:25–34."
 
-dataRaw = data;
+dataRaw = data; % not used
 
 dataIsCell = iscell(data);
 
 if ~dataIsCell
-[~,dataDim] = max(size(data));
-data = num2cell(data,dataDim);
+    [~,dataDim] = max(size(data));
+    data = num2cell(data,dataDim);
 end
 
 si = 1/SR;
@@ -31,40 +31,40 @@ filterfactor = (1-exp(-2*pi*si*fc));
 
 dataCorrected = cell(size(data));
 
-for iTrace = 1:numel(data) 
-nPoints = length(data{iTrace});
-
-Vlast = Vhold-data{iTrace}(1)*Rs; % initialize DC voltage at membrane
-
-denominator = Vlast-Vrev;
-
-if denominator ~= 0 
-    fracCorr = fraction*(1-(Vhold-Vrev)/denominator);
-else
-    fracCorr = 0;
-end
-data{iTrace}(1) = data{iTrace}(1)*(1-fracCorr); % DC correction for first data point
-
-
-for i=2:nPoints-1 % loop through all other points
-   Vthis = Vhold - data{iTrace}(i)*Rs;
-   if Vthis ~= Vrev
-       fracCorr = fraction*(1-(Vhold-Vrev)/(Vthis-Vrev));
-   else
-       fracCorr = 0;
-   end
-       Icap = Cm*(Vthis-Vlast)/si;
-       Icap = Icap*filterfactor;
-       data{iTrace}(i-1) = data{iTrace}(i-1)-(fraction*Icap);
-       data{iTrace}(i-1) = data{iTrace}(i-1)*(1-fracCorr);
-       Vlast = Vthis;
-end
-
-
+for iTrace = 1:numel(data)
+    nPoints = length(data{iTrace});
+    
+    Vlast = Vhold-data{iTrace}(1)*Rs; % initialize DC voltage at membrane
+    
+    denominator = Vlast-Vrev;
+    
+    if denominator ~= 0
+        fracCorr = fraction*(1-(Vhold-Vrev)/denominator);
+    else
+        fracCorr = 0;
+    end
+    data{iTrace}(1) = data{iTrace}(1)*(1-fracCorr); % DC correction for first data point
+    
+    
+    for i=2:nPoints-1 % loop through all other points
+        Vthis = Vhold - data{iTrace}(i)*Rs;
+        if Vthis ~= Vrev
+            fracCorr = fraction*(1-(Vhold-Vrev)/(Vthis-Vrev));
+        else
+            fracCorr = 0;
+        end
+        Icap = Cm*(Vthis-Vlast)/si;
+        Icap = Icap*filterfactor;
+        data{iTrace}(i-1) = data{iTrace}(i-1)-(fraction*Icap);
+        data{iTrace}(i-1) = data{iTrace}(i-1)*(1-fracCorr);
+        Vlast = Vthis;
+    end
+    
+    
 end
 
 if ~dataIsCell
-dataCorrected = cell2mat(data);
+    dataCorrected = cell2mat(data);
 end
 
 
